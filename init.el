40e82a1afb9fe1ee7f7e09f070f7ce5c2aacc65a
danyaltairoski <139523729+danyaltairoski@users.noreply.github.com>
2025-09-22 07:34:19 -0400

    Add files via upload

--- init.el
+++ init.el
@@ -5,7 +5,6 @@
 ;;------------------------------------------------------------------------------
 
 (setq inhibit-startup-message t)          ; no startup screen
-(add-to-list 'default-frame-alist '(fullscreen . maximized))   ;; maximized
 (setq confirm-kill-emacs 'y-or-n-p)       ; ask before exiting
 (setq default-directory "~/Desktop/")     ; default C-x C-f dir
 (setq auto-save-default nil)              ; don't auto-save
@@ -22,6 +21,15 @@
 (load custom-file 'noerror)
 
 ;;------------------------------------------------------------------------------
+;; Ensure Emacs sees your shell PATH (macOS only)
+;;------------------------------------------------------------------------------
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+;;------------------------------------------------------------------------------
 ;; UI Tweaks
 ;;------------------------------------------------------------------------------
 
@@ -30,7 +38,7 @@
 (tooltip-mode -1)
 (menu-bar-mode -1)
 (set-fringe-mode 10)
-(set-face-attribute 'default nil :height 175)
+(set-face-attribute 'default nil :height 160)
 
 (column-number-mode)
 (global-display-line-numbers-mode t)
@@ -52,9 +60,6 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org"   . "https://orgmode.org/elpa/")
                          ("elpa"  . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
@@ -66,9 +71,12 @@
 ;; Appearance (Themes & Modeline)
 ;;------------------------------------------------------------------------------
 
+(use-package all-the-icons
+  :if (display-graphic-p))
+
 (use-package doom-themes
   :config
-  (load-theme 'doom-tomorrow-day t))
+  (load-theme 'doom-one-light t))
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
@@ -90,7 +98,7 @@ to tell appearance preferences to get dark mode' 2>/dev/null"))))
   (let ((dark-mode (my/macos-dark-mode-p)))
     (unless (eq dark-mode my/macos-last-dark-mode)
       (mapc #'disable-theme custom-enabled-themes)
-      (load-theme (if dark-mode 'doom-tomorrow-night 'doom-tomorrow-day) t)
+      (load-theme (if dark-mode 'doom-one 'doom-one-light) t)
       (setq my/macos-last-dark-mode dark-mode))))
 
 (my/macos-theme-sync)
@@ -207,16 +215,7 @@ to tell appearance preferences to get dark mode' 2>/dev/null"))))
         ("~/Desktop/org/resources.org" :maxlevel . 2)
         ("~/Desktop/org/shopping.org"  :maxlevel . 1)))
 
-;;------------------------------------------------------------------------------
-;; Programming Languages
-;;------------------------------------------------------------------------------
 
-;; Assembly
-(add-to-list 'auto-mode-alist '("\\.asm\\'" . asm-mode))
-(add-to-list 'auto-mode-alist '("\\.s\\'"   . asm-mode))
-(setq asm-comment-char ?#) ;; or ?\; for NASM
-
-(use-package riscv-mode :mode "\\.s\\'")
 
 ;;------------------------------------------------------------------------------
 ;; Convenience
@@ -229,3 +228,164 @@ to tell appearance preferences to get dark mode' 2>/dev/null"))))
 
 (global-set-key (kbd "C-c I") 'open-init-file)
 
+;;------------------------------------------------------------------------------
+;; Projectile - project management
+;;------------------------------------------------------------------------------
+
+(use-package projectile
+  :diminish projectile-mode
+  :init
+  (setq projectile-completion-system 'ivy)
+  :config
+  (projectile-mode 1)
+  (setq projectile-project-search-path '("~/Desktop/projects"))
+  (setq projectile-switch-project-action #'projectile-dired)
+  :bind-keymap
+  ("C-c p" . projectile-command-map))
+
+(use-package counsel-projectile
+  :after (projectile counsel)
+  :config (counsel-projectile-mode 1))
+
+;;------------------------------------------------------------------------------
+;; Magit - Git interface
+;;------------------------------------------------------------------------------
+(use-package magit
+  :ensure t
+  :commands (magit-status magit-get-current-branch)
+  :bind
+  ("C-x g" . magit-status))  ;; common shortcut to open Magit
+
+;;------------------------------------------------------------------------------
+;; LSP Mode (Language Server Protocol)
+;;------------------------------------------------------------------------------
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook ((python-mode . lsp-deferred)
+         (js-mode . lsp-deferred)
+         (typescript-mode . lsp-deferred)
+         (c-mode . lsp-deferred)
+         (c++-mode . lsp-deferred)
+         (java-mode . lsp-deferred)
+         (latex-mode . lsp-deferred)
+         (html-mode . lsp-deferred)
+         (css-mode . lsp-deferred))
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; keymap for lsp commands
+  :config
+  (lsp-enable-which-key-integration t))
+
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+;; Optional: UI enhancements for LSP ------------------------------
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+;; Treemacs
+(use-package treemacs
+  :ensure t)
+
+;; VTerm
+(use-package vterm
+  :ensure t)
+
+(use-package lsp-ivy)
+
+;; Which-key integration (helps you discover keybindings)
+(use-package which-key
+  :config
+  (which-key-mode))
+
+;; Company mode for autocompletion --------------------------------
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+;;------------------------------------------------------------------------------
+;; Programming Languages
+;;------------------------------------------------------------------------------
+
+;; Assembly
+(add-to-list 'auto-mode-alist '("\\.asm\\'" . asm-mode))
+(add-to-list 'auto-mode-alist '("\\.s\\'"   . asm-mode))
+(setq asm-comment-char ?#) ;; or ?\; for NASM
+(setq lsp-asm-lsp-executable "asm-lsp")
+
+(add-hook 'asm-mode-hook
+          (lambda ()
+            (setq tab-width 4)
+            (setq indent-tabs-mode nil)))
+
+;; Web Development
+;; Use web-mode for HTML, CSS, and JS files
+(use-package web-mode
+  :mode ("\\.html?\\'" "\\.css\\'" "\\.js\\'")
+  :hook (web-mode . lsp-deferred)
+  :config
+  ;; indentation settings
+  (setq web-mode-markup-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-css-indent-offset 2)
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-current-element-highlight t)) ;; highlights current tag)
+
+;; Emmet-mode for fast HTML/CSS expansions
+(use-package emmet-mode
+  :hook (web-mode css-mode html-mode))
+
+;; Prettier for auto-formatting JS/TS/CSS/HTML
+(use-package prettier-js
+  :hook ((js-mode . prettier-js-mode)
+         (typescript-mode . prettier-js-mode)
+         (css-mode . prettier-js-mode)
+         (web-mode . prettier-js-mode)))
+
+;;-----------
+;; PDF Support
+;;-------------
+
+(use-package pdf-tools
+  :ensure t
+  :config
+  (pdf-tools-install)
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))))   
+
+(add-hook 'pdf-view-mode-hook
+          (lambda ()
+            (display-line-numbers-mode 0)))
+
+(setq insert-directory-program "gls")
+
+(defun open-in-firefox ()
+  "Open the current file in Firefox on macOS."
+  (interactive)
+  (when buffer-file-name
+    (save-buffer) ;; save before opening
+    (start-process "firefox" nil "open" "-a" "Firefox" buffer-file-name)))
+
+(global-set-key (kbd "C-c f") 'open-in-firefox)
+
+;; remove annoying lsp snippet
+(setq lsp-auto-guess-root t)
+(setq lsp-session-file (expand-file-name ".lsp-session-v1" user-emacs-directory))
+(setq lsp-enable-snippet nil) ;; optional
+
+(setq TeX-PDF-mode t)        ;; compile directly to PDF
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil) ;; query for master file
